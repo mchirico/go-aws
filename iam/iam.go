6bd1e7a4e488092973b922588f9619df52c66361
Mike Chirico (https://mchirico.github.io/) <mchirico@gmail.com>
2022-07-11 16:55:50 -0400

    update-automated-save

--- iam/iam.go
+++ iam/iam.go
@@ -16,6 +16,27 @@ func CreateKey(cfg aws.Config, userName string) (*iam.CreateAccessKeyOutput, err
 
 }
 
+func DeleteAccessKey(cfg aws.Config, userName, keyId string) (*iam.DeleteAccessKeyOutput, error) {
+	client := iam.NewFromConfig(cfg)
+	input := &iam.DeleteAccessKeyInput{
+		AccessKeyId: new(string),
+		UserName:    new(string),
+	}
+	return client.DeleteAccessKey(context.TODO(), input)
+
+}
+
+func AddUserToGroup(cfg aws.Config, userName, groupName string) (*iam.AddUserToGroupOutput, error) {
+	client := iam.NewFromConfig(cfg)
+	input := &iam.AddUserToGroupInput{
+		GroupName: &groupName,
+		UserName:  &userName,
+	}
+
+	return client.AddUserToGroup(context.TODO(), input)
+
+}
+
 func ListUsers(cfg aws.Config, input *iam.ListUsersInput) (*iam.ListUsersOutput, error) {
 	client := iam.NewFromConfig(cfg)
 	return client.ListUsers(context.TODO(), input)
@@ -25,4 +46,14 @@ func ListUsers(cfg aws.Config, input *iam.ListUsersInput) (*iam.ListUsersOutput,
 func CreateUser(cfg aws.Config, input *iam.CreateUserInput) (*iam.CreateUserOutput, error) {
 	client := iam.NewFromConfig(cfg)
 	return client.CreateUser(context.TODO(), input)
+}
+
+func DeleteUser(cfg aws.Config, input *iam.DeleteUserInput) (*iam.DeleteUserOutput, error) {
+	client := iam.NewFromConfig(cfg)
+	return client.DeleteUser(context.TODO(), input)
+}
+
+func RemoveUserFromGroup(cfg aws.Config, input *iam.RemoveUserFromGroupInput) (*iam.RemoveUserFromGroupOutput, error) {
+	client := iam.NewFromConfig(cfg)
+	return client.RemoveUserFromGroup(context.TODO(), input)
 }
--- iam/iam_test.go
+++ iam/iam_test.go
@@ -5,6 +5,7 @@ import (
 	"testing"
 
 	"github.com/aws/aws-sdk-go-v2/service/iam"
+	"github.com/aws/aws-sdk-go-v2/service/iam/types"
 	"github.com/mchirico/go-aws/client"
 )
 
@@ -20,5 +21,80 @@ func TestListUsers(t *testing.T) {
 	for _, v := range result.Users {
 		fmt.Println(*v.UserName)
 	}
+
+}
+
+func TestCreateUsers(t *testing.T) {
+	userName := "go-aws"
+	key := "Name"
+	value := "go-aws"
+	tag := types.Tag{Key: &key, Value: &value}
+	input := &iam.CreateUserInput{
+		UserName: &userName,
+		Tags:     []types.Tag{tag},
+	}
+	result, err := CreateUser(client.Config(), input)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	fmt.Println(result)
+
+}
+
+func TestCreateKey(t *testing.T) {
+	userName := "go-aws"
+
+	result, err := CreateKey(client.Config(), userName)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	fmt.Println(result)
+
+}
+
+func TestAddUserToGroup(t *testing.T) {
+	userName := "go-aws"
+	groupName := "Admin"
+
+	result, err := AddUserToGroup(client.Config(), userName, groupName)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	fmt.Println(result)
+
+}
+
+func Test_RemoveUserFromGroup(t *testing.T) {
+
+	userName := "go-aws"
+	groupName := "Admin"
+	input := &iam.RemoveUserFromGroupInput{
+		GroupName: &groupName,
+		UserName:  &userName,
+	}
+	result, err := RemoveUserFromGroup(client.Config(), input)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	fmt.Println(result)
+
+}
+
+func TestDeleteUser(t *testing.T) {
+	userName := "go-aws"
+
+	input := &iam.DeleteUserInput{
+		UserName: &userName,
+	}
+	result, err := DeleteUser(client.Config(), input)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	fmt.Println(result)
 
 }
