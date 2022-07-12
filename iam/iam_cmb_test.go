2a094f6436d709edfbc896a5241f3f5b7552b4dd
Mike Chirico (https://mchirico.github.io/) <mchirico@gmail.com>
2022-07-12 10:55:29 -0400

    update-automated-save

---
+++ iam/iam_cmb_test.go
@@ -0,0 +1,42 @@
+/*
+iam_cmb ... iam combination. FIXME (mmc), come up with
+a better name.
+
+*/
+
+package iam
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestWriteFile(t *testing.T) {
+	data := []byte("junk\n1\n1")
+	file := "/Users/mchirico/junk2.txt"
+	err := WriteFile(file, data)
+	if err != nil {
+		t.Fatal(err)
+	}
+}
+
+func TestI_ListAccessKeys(t *testing.T) {
+	i := NewI("k8s")
+	result, err := i.ListAccessKeys()
+	if err != nil {
+		t.Fatal(err)
+	}
+	fmt.Println(result)
+}
+
+func TestI_ListAttachedGroupPolicies(t *testing.T) {
+	i := NewI("k8s")
+	result, err := i.ListAttachedGroupPolicies("k8s")
+	if err != nil {
+		t.Fatal(err)
+	}
+	for _, v := range result.AttachedPolicies {
+		fmt.Println(*v.PolicyName, *v.PolicyArn)
+	}
+
+}
