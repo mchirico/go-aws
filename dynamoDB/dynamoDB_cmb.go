487c25ade983f00db7a97afe1a16c414f260bc00
Mike Chirico (https://mchirico.github.io/) <mchirico@gmail.com>
2022-07-17 17:28:09 -0400

    update-automated-save

--- dynamoDB/dynamoDB_cmb.go
+++ dynamoDB/dynamoDB_cmb.go
@@ -5,23 +5,83 @@ import (
 
 	"github.com/aws/aws-sdk-go-v2/aws"
 
+	"github.com/aws/aws-sdk-go-v2/feature/dynamodb/attributevalue"
 	"github.com/aws/aws-sdk-go-v2/service/dynamodb"
 )
 
-type D struct {
+type DB struct {
 	name string
 	cfg  aws.Config
 	max  int32
 }
 
-func NewD() *D {
-	return &D{cfg: client.Config(), max: int32(50)}
+func NewDB(name string) *DB {
+	return &DB{
+		name: name,
+		cfg:  client.Config(),
+		max:  int32(50)}
 }
 
-func (d *D) List() (*dynamodb.ListTablesOutput, error) {
+func (d *DB) List() (*dynamodb.ListTablesOutput, error) {
 	input := &dynamodb.ListTablesInput{
 		Limit: &d.max,
 	}
 	return List(d.cfg, input)
 
+}
+
+func (d *DB) Put(pkey, skey, status string, doc *Doc) error {
+
+	p := &PKSK{}
+	p.PK = pkey
+	p.SK = skey
+	p.Status = status
+	p.Doc = *doc
+
+	av, err := attributevalue.MarshalMap(p)
+	if err != nil {
+		return err
+	}
+	_, err = Put(d.cfg, d.name, av)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (d *DB) Get(pkey, skey string) (*PKSK, error) {
+
+	type KEY struct {
+		PK string `json:"PK"`
+		SK string `json:"SK"`
+	}
+
+	key, _ := attributevalue.MarshalMap(&KEY{
+		PK: pkey,
+		SK: skey,
+	})
+
+	input := &dynamodb.GetItemInput{
+		Key:             key,
+		TableName:       &d.name,
+		AttributesToGet: []string{"PK", "Doc", "SK", "Status"},
+	}
+	result, err := Get(d.cfg, input)
+	if err != nil {
+		return nil, err
+	}
+	p := &PKSK{}
+	err = attributevalue.UnmarshalMap(result.Item, p)
+	if err != nil {
+		return nil, err
+	}
+	return p, nil
+
+}
+
+func (d *DB) Doc(location, aws string) *Doc {
+	doc := &Doc{}
+	doc.Location = location
+	doc.AWS = aws
+	return doc
 }
--- dynamoDB/dynamoDB_cmb_test.go
+++ dynamoDB/dynamoDB_cmb_test.go
@@ -2,14 +2,46 @@ package dynamoDB
 
 import (
 	"fmt"
+	"github.com/aws/aws-sdk-go-v2/feature/dynamodb/attributevalue"
 	"testing"
 )
 
 func TestD_List(t *testing.T) {
-	d := NewD()
+	d := NewDB("mmcPKSK")
 	result, err := d.List()
 	if err != nil {
 		t.Fatal(err)
 	}
-	fmt.Println(result)
+	for _, table := range result.TableNames {
+		fmt.Println(table)
+	}
+
+}
+
+func TestD_Put(t *testing.T) {
+	pkey := "TestD_Put"
+	skey := "skey:TestD_Put"
+
+	d := NewDB("mmcPKSK")
+	p := &PKSK{}
+	p.PK = pkey
+	p.SK = skey
+	p.Status = "Good"
+	p.Doc = *d.Doc("location", "string")
+	av, err := attributevalue.MarshalMap(p)
+	if err != nil {
+		t.Fatal(err)
+	}
+	_, err = Put(d.cfg, d.name, av)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	result, err := d.Get(pkey, skey)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if result.PK != pkey || result.SK != skey {
+		t.Fatal("Get failed")
+	}
 }
